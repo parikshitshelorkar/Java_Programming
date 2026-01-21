7fe8fc2fb933221315c5b04a69e05180ec0cf8ff
parikshitshelorkar <parikshitshelorkar2.0@gmail.com>
2026-01-21 21:48:53 +0530

    Adding Task files

---
+++ JAVA_FullStack/Tasks/Employee.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Employee{
+
+    //Data Members
+    int empId, salary;
+    String name, gender;
+
+    //Member Function
+    void getData(){
+        
+    }
+    void showBonus(){
+
+    }
+}
