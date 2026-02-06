793e0c688dc5a77a5b527b14aba680fc9cd9c2b2
parikshitshelorkar <parikshitshelorkar2.0@gmail.com>
2026-02-06 20:10:12 +0530

    advanced folder added

---
+++ Advanced/Main.java
@@ -0,0 +1,40 @@
+
+import java.lang.reflect.Method;
+
+// Student class with two methods
+class Student {
+    public void study() {
+        System.out.println("Student is studying...");
+    }
+
+    public void giveExam(String subject) {
+        System.out.println("Student is giving exam in " + subject);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            // Load class
+            Class<?> cls = Class.forName("Student");
+
+            // Create object
+            Object obj = cls.getDeclaredConstructor().newInstance();
+
+            // Call study() method
+            Method m1 = cls.getMethod("study");
+            m1.invoke(obj);
+
+            // Call giveExam(String) method
+            Method m2 = cls.getMethod("giveExam", String.class);
+            m2.invoke(obj, "Mathematics");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
